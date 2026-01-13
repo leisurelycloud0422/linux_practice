503e672292f31fdd336d760857b234fd5fd761ac
eric <eerriicc11112200@gmail.com>
2026-01-13 08:52:48 +0800

    optimize platform_driver and try function

--- platform_driver/platform_device.c
+++ platform_driver/platform_device.c
@@ -1,11 +1,19 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 
-static struct platform_device my_fake_device = {
-    .name = "my_platform_driver",  // 要與 driver 中 .driver.name 相同
-    .id = -1,
-};
+static void my_device_release(struct device *dev)
+{
+    pr_info("my_platform_device: device.release() called\n");
+}
 
+static struct platform_device my_fake_device = {
+    .name = "my_platform_driver",
+    .id = -1,
+    // 2. 在這裡綁定它
+    .dev = {
+        .release = my_device_release,
+    },
+};
 static int __init my_device_init(void)
 {
     return platform_device_register(&my_fake_device);
@@ -13,6 +21,7 @@ static int __init my_device_init(void)
 
 static void __exit my_device_exit(void)
 {
+    pr_info("my_platform_device: unregistering device\n");
     platform_device_unregister(&my_fake_device);
 }
 
